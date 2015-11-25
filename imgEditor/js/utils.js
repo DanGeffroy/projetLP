2350b8f6fcace0de301a24705bff59d001d5125b
Dan Geffroy <dan.geffroy@etu.univ-nantes.fr>
2015-11-25 13:35:36 +0100

    Correcting code + canvas size

--- imgEditor/js/utils.js
+++ imgEditor/js/utils.js
@@ -5,7 +5,7 @@ var getRandomInt=fabric.util.getRandomInt;function getRandomColor(){return(pad(g
 pad(getRandomInt(0,255).toString(16),2)+
 pad(getRandomInt(0,255).toString(16),2));}
 function getRandomNum(min,max){return Math.random()*(max- min)+ min;}
-function getRandomLeftTop(){var offset=50;return{left:fabric.util.getRandomInt(0+ offset,700- offset),top:fabric.util.getRandomInt(0+ offset,500- offset)};}
+function getRandomLeftTop(){var offset=50;return{left:fabric.util.getRandomInt(0+ offset,417.5- offset),top:fabric.util.getRandomInt(0+ offset,562.5- offset)};}
 var supportsInputOfType=function(type){return function(){var el=document.createElement('input');try{el.type=type;}
 catch(err){}
 return el.type===type;};};var supportsSlider=supportsInputOfType('range'),supportsColorpicker=supportsInputOfType('color');global.getRandomNum=getRandomNum;global.getRandomInt=getRandomInt;global.getRandomColor=getRandomColor;global.getRandomLeftTop=getRandomLeftTop;global.supportsSlider=supportsSlider;global.supportsColorpicker=supportsColorpicker;global.capitalize=capitalize;})(this);
